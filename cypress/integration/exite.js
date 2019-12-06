d885a3de7c161f0c5c50ebdf0e1e04c0159d5fcd
pishtik <31575898+pishtik@users.noreply.github.com>
2019-12-06 14:12:50 +0100

    Create exite.js

---
+++ cypress/integration/exite.js
@@ -0,0 +1,28 @@
+describe('Demo of cy login test', function() {
+  it('Exite login test', function() {
+    cy.visit('https://lxedidexiqa01.tsys.editel.eu')
+	
+	it('can submit a valid form', function () {
+	cy.log('filling out user name and password') // if you really need this
+	
+	cy.get('input#identifier').type('user')
+	  .get('input#password').type('password')
+	  
+	cy.contains('Sign in').click()
+	
+	cy.get('.editel-header').contains('Signed in as')
+							.contains('user')
+	cy.url().should('include', '/exite2-gui/home')
+	
+	cy.log('verifing logout button')
+	
+	cy.get('#menu_logout').should('have.attr', 'href', '/exite2-gui/logout')
+						  .click()
+						  
+	cy.url().should('include', '/auth-server/login')
+	
+	cy.get('.editel-exitelogo-svg').should('be.visible')
+	
+	cy.screenshot()
+  })
+})
